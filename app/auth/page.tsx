6fdfc214200a3a82ebca6ff52529a09cf2ffc1cb
magnusgbjerke <magnusgbjerke@gmail.com>
2025-04-30 15:58:39 +0200

    commit

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,10 +2,11 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Page = () => {
   const { data: session } = useSession();
+  const [state, setState] = useState("");
 
   useEffect(() => {
     if (session?.error === "RefreshAccessTokenError") {
@@ -14,7 +15,7 @@ const Page = () => {
   }, [session]);
 
   async function handleClick() {
-    const url = "http://localhost:8080/api/user/user-info";
+    const url = "http://localhost:8080/api/user-info3";
 
     let headers = {};
 
@@ -35,7 +36,7 @@ const Page = () => {
         headers: headers,
       });
       const product = await response.json();
-      console.log("Product data:", product);
+      setState(JSON.stringify(product));
     } catch (error) {
       console.error(error);
     }
@@ -47,13 +48,24 @@ const Page = () => {
         <>
           <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
           <p className="text-lg mb-6">Please sign in to continue.</p>
-          <button onClick={handleClick}>test</button>
+
           <button
             onClick={() => signIn("keycloak")}
             className="px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Sign in
           </button>
+          <br />
+          <button
+            onClick={handleClick}
+            className="mt-4 mb-4 px-6 py-3 bg-orange-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Fetch from server
+          </button>
+
+          <p>
+            Data from server: <br /> {state}
+          </p>
           <div className="mt-6">
             <p className="text-lg">
               New user?{" "}
@@ -69,9 +81,21 @@ const Page = () => {
       ) : (
         <>
           <p>Signed in as {session?.user?.name}</p>
-          <button onClick={handleClick}>test</button>
+          <button
+            onClick={handleClick}
+            className="mt-4 mb-4 px-6 py-3 bg-orange-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Fetch from server
+          </button>
           <br />
-          <button onClick={() => signOut({ callbackUrl: "/" })}>
+          <p>
+            Data from server: <br /> {state}
+          </p>
+          <br />
+          <button
+            onClick={() => signOut({ callbackUrl: "/auth" })}
+            className="px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
             Sign Out
           </button>
         </>
